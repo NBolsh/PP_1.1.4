7d4b08e88452cace71fb74f98cb9e58d38acdeac
Nikita Bolshakov <bolshackov-99@mail.ru>
2023-09-03 23:44:40 +0700

    Initial commit

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,80 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    private final static String CREATING = "CREATE TABLE Users (userId INT PRIMARY KEY AUTO_INCREMENT, userName VARCHAR(45), userLastName VARCHAR(45), userAge TINYINT(3))";
+    private final static String INSERT = "INSERT INTO Users (userName, userLastName, userAge) VALUES (?, ?, ?)";
+    private final static String DELETING = "DELETE FROM Users  WHERE userId = ?";
+    private final static String GET_ALL = "SELECT * FROM Users";
+
+    public UserDaoJDBCImpl() {
+    }
+@Override
+    public void createUsersTable() {
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.executeUpdate(CREATING);
+        } catch (SQLException e) {
+        }
+    }
+@Override
+    public void dropUsersTable() {
+        try(Statement statement = Util.getConnection().createStatement()) {
+            String sqlCommand = "DROP TABLE Users";
+            statement.execute(sqlCommand);
+        } catch (SQLException e) {
+        }
+
+    }
+@Override
+    public void saveUser(String name, String lastName, byte age) {
+        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(INSERT)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем - " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+@Override
+    public void removeUserById(long id) {
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(DELETING)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+@Override
+    public List<User> getAllUsers() {
+        List<User> userList = new ArrayList<>();
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(GET_ALL)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                User user = new User(resultSet.getString("userName"),
+                        resultSet.getString("userLastName"),
+                        resultSet.getByte("userAge"));
+                userList.add(user);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return userList;
+    }
+@Override
+    public void cleanUsersTable() {
+        try (Statement statement = Util.getConnection().createStatement()) {
+            String sqlCommand = "TRUNCATE Users";
+            statement.executeUpdate(sqlCommand);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private final static String URL = "jdbc:mysql://localhost:3306/mydatabasekata";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "root";
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
