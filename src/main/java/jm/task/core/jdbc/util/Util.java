91124e44ee19c2dcba8050555b7bf55f0f0b64c2
Nikita Bolshakov <bolshackov-99@mail.ru>
2023-09-11 11:58:19 +0700

    Second util update

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,16 +6,16 @@ import java.sql.SQLException;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
 import java.util.Properties;
 
 public class Util {
-    private final static String URL = "jdbc:mysql://localhost:3306/mydatabasekata?useSSL=false";
-    private final static String USERNAME = "root";
-    private final static String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabasekata?useSSL=false";
+    private static final  String USERNAME = "root";
+    private static final String PASSWORD = "root";
     private static Connection connection;
     private static SessionFactory sessionFactory;
 
@@ -38,17 +38,17 @@ public class Util {
                 Configuration configuration = new Configuration();
 
                 Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, URL);
-                settings.put(Environment.USER, USERNAME);
-                settings.put(Environment.PASS, PASSWORD);
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                settings.put(AvailableSettings.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(AvailableSettings.URL, URL);
+                settings.put(AvailableSettings.USER, USERNAME);
+                settings.put(AvailableSettings.PASS, PASSWORD);
+                settings.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
 
-                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(AvailableSettings.SHOW_SQL, "true");
 
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-                settings.put(Environment.HBM2DDL_AUTO, "");
+                settings.put(AvailableSettings.HBM2DDL_AUTO, "");
 
                 configuration.setProperties(settings);
 
